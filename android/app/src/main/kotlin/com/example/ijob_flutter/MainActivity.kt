fd170989f57a77f010a3dba56d849005274ef619
Manny Talaroc <magnezyle@gmail.com>
2023-08-28 10:39:46 +0800

    initial commit

---
+++ android/app/src/main/kotlin/com/example/ijob_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ijob_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Services/global_variables.dart
@@ -0,0 +1,2 @@
+
+String loginUrlImage = 'https://www.10wallpaper.com/wallpaper/3840x2160/1807/Business_Architecture_Buildings_4K_Photo_3840x2160.jpg';
